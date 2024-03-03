1c88f3629cbec57e6d020859bd3b27ef7d0cf87e
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-03-03 15:19:01 +0500

    added CssXpathExamples

---
+++ src/test/java/CssXpathExamples.java
@@ -0,0 +1,70 @@
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class CssXpathExamples {
+    void cssXpathExamples(){
+        // Самый лучший способ добраться до элемента, это по параметру "data-testid", который разработчик должен
+        // добавлять в элементы. Данный параметр предназначен для автотестировщиков
+
+        // <input type="email" class="inputtext login_form_input_box" name="email" id="email" data-testid="email">
+
+        $("[data-testid=email]").setValue("1");
+        // OR
+        $(by("data-testid", "email")).setValue("1");
+
+        // Если данного параметра у элемента нет, то обращаемся по "id"
+
+        // <input type="email" class="inputtext login_form_input_box" name="email" id="email">
+        $("[id=email]").setValue("1");
+        // OR
+        $("#email").setValue("1");
+        // OR
+        $("[input#email").setValue("1");
+        // By xpath
+        $x("//*[@id='email']").setValue("1");
+        $x("//input[@id='email']").setValue("1");
+
+        // Если нет "id", то используем "name"
+
+        // <input type="email" class="inputtext login_form_input_box" name="email">
+        $("[name=email]").setValue("1");
+        // OR
+        $(byName("email")).setValue("1");
+
+        // Если нет "name", то используем "class"
+
+        // <input type="email" class="inputtext login_form_input_box">
+        $("[class=login_form_input_box]").setValue("1");
+        // OR
+        $(".login_form_input_box").setValue("1");
+        // OR
+        $(".inputtext.login_form_input_box").setValue("1");
+        // OR
+        $("input.inputtext.login_form_input_box").setValue("1");
+        // OR by xpath
+        $x("//input[@class='inputtext'][@class='login_form_input_box']").setValue("1");
+
+        // !По параметру "type" искать не рекомендуется
+
+        // Для вложенных элементов
+        // <div class="inputtext">
+        //      <input type="email" class="login_form_input_box">
+        // </div>
+
+        $(".inputtext").$("login_form_input_box").setValue("1");
+        // OR
+        $(".inputtext .login_form_input_box").setValue("1");
+
+        // Поиск по тексту
+        // <div>Hello, QA guru!</div>
+
+        $x("//div[text()='Hello, QA guru!']").click();
+        // OR
+        $(byText("Hello, QA guru!")).click();
+        // Также можно найти тест по соответствию
+        $(withText("llo, QA gu")).click();
+
+
+    }
+}
