45f0350aa337e3357ad385c97380c23a2cdf4fbe
Ruslan Bogoutdinov <rusyabogoutdinov97@gmail.com>
2024-03-07 22:36:18 +0500

    added OtherMethods

---
+++ src/test/java/OtherMethods.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+
+public class OtherMethods {
+    @Test
+    void otherMethodsTest(){
+        // Ищем по частичному совпадению, игнорируя кейс
+        $("#someElement").shouldHave(text("abs"));
+        // Ищем по точному совпадению, игнорируя кейс
+        $("#someElement").shouldHave(exactText("abs"));
+
+        // Ищем по частичному совпадению, не игнорируя кейс
+        $("#someElement").shouldHave(textCaseSensitive("abs"));
+        // Ищем по точному совпадению, не игнорируя кейс
+        $("#someElement").shouldHave(exactTextCaseSensitive("abs"));
+
+        // Поиск элемента с классом более одного
+        // Поиск элемента с классом внутри другого элемента с определенным классом
+        $(".classOne .classTwo");
+
+        // Поиск элемента с двойным классом
+        $(".classOne.classTwo");
+    }
+}
